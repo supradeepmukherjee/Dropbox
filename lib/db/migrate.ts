51a95ada1d0a3edeb30cbbca8b2d565ccaa868ab
Supradeep Mukherjee <supradeep2004@gmail.com>
2025-04-26 16:00:42 +0530

    migration fix

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -1,6 +1,9 @@
 import { neon } from "@neondatabase/serverless"
+import { config } from "dotenv"
 import { drizzle } from "drizzle-orm/neon-http"
 import { migrate } from "drizzle-orm/neon-http/migrator"
+
+config()
 
 if (!process.env.DATABASE_URL) throw new Error('DB Url not found in .env')
 
