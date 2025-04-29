bfc6477aa4bf10e4c284d5cdc9d6e8159d4f1984
Supradeep Mukherjee <supradeep2004@gmail.com>
2025-04-29 21:29:35 +0530

    HeroUI Provider & using in layout

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,28 @@
+'use client'
 import { ReactNode } from "react"
 import { ThemeProviderProps } from 'next-themes' // check if type has to be imported
+import { ImageKitProvider } from 'imagekitio-next'
+import { HeroUIProvider } from "@heroui/react"
 
 const Providers = ({ children }: ProviderProps) => (
-    <h1>
-        {children}
-    </h1>
+    <ImageKitProvider
+        publicKey={process.env.NEXT_PUBLIC_IMGKIT_PUBLIC_KEY}
+        urlEndpoint={process.env.NEXT_PUBLIC_IMGKIT_URL}
+        authenticator={async () => {
+            try {
+                const res = await fetch('/api/imgkit-auth')
+                const data = await res.json()
+                return data
+            } catch (err) {
+                console.log(err)
+                throw err
+            }
+        }}
+    >
+        <HeroUIProvider>
+            {children}
+        </HeroUIProvider>
+    </ImageKitProvider>
 )
 
 export interface ProviderProps {
